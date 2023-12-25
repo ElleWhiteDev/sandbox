85e3776d6e090cebaa72ba97d3f2a7cdcaad30a9
Elizabeth White <redfragle88@gmail.com>
2023-12-25 10:15:39 -0500

    The blizzard has arrived

---
+++ snowfall.py
@@ -0,0 +1,55 @@
+import os
+import random
+import time
+
+SNOW_DENSTITY = 5
+DELAY = .5
+
+snowflakes = ['❄️', '❅', '❆','❉','❊']
+
+term = os.get_terminal_size()
+
+w = term.columns
+h = term.lines
+
+grid = []
+
+for _ in range(h):
+    # can sub letter for space to check if working
+    grid.append([' ' * w])
+
+
+def draw_grid():
+    """
+    _summary_: Draws the grid
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print('\033[?25l')
+
+    output = ''
+
+    for row in grid:
+        output += ''.join(row) + '\n'
+
+    output = output.strip('\n')
+
+    print(output, end='')
+
+
+while True:
+    # add snowflakes to grid
+    row = []
+
+    for _ in range(w):
+        if random.random() < SNOW_DENSTITY / 100:
+            row.append(random.choice(snowflakes))
+        else:
+            row.append(' ')
+
+    grid.insert(0, row)
+    grid.pop()
+
+    draw_grid()
+
+    time.sleep(DELAY)
